7ef0dbc9ebece9e617c4412faa712ffc47a7d404
ik-fib <ik11235@gmail.com>
2014-03-17 21:45:54 +0900

    make first function init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-readlink $0
+cd `dirname $0`
+#pwd
+for FILE in ".emacs" ".emacs.d"
+do
+    ln -s `pwd`/${FILE} ${HOME}/${FILE}
+done
+#$ find . -mindepth 1  -maxdepth 1 ! -name .git -a ! -name README.md -print0 |sed -e s/\.// |xargs -0 -I % echo "ln -s `pwd`/% ${HOME}/%"
