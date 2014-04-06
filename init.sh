30884b39bf4752c5e00e6ef3b5b66ee782c466be
ik-fib <ik11235@gmail.com>
2014-04-06 15:22:39 +0900

    add init-loader.el download

--- init.sh
+++ init.sh
@@ -5,4 +5,6 @@ for FILE in ".emacs.d" ".zshrc"
 do
     ln -s `pwd`/${FILE} ${HOME}/${FILE}
 done
+cd ".emacs.d/side-lisp"
+curl -O https://raw.githubusercontent.com/emacs-jp/init-loader/master/init-loader.el
 #$ find . -mindepth 1  -maxdepth 1 ! -name .git -a ! -name README.md -print0 |sed -e s/\.// |xargs -0 -I % echo "ln -s `pwd`/% ${HOME}/%"
