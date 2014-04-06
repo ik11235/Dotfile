500a2e2a8f02bde9589953fba59ab9866a8f0bdd
ik-fib <ik11235@gmail.com>
2014-04-07 07:10:53 +0900

    fix magit-config and add magit-config comment

--- .emacs.d/inits/cocoa-emacs-magit.el
+++ .emacs.d/inits/cocoa-emacs-magit.el
@@ -1,1 +1,3 @@
-(set-variable 'magit-emacsclient-executable "/Applications/Emacs.app/Contents/MacOS/Emacs")
+;brew経由のemacsをGUIで起動した際、そのままではcommit messageを入力できないため、以下で対応
+;;http://qiita.com/dtan4/items/658a8a7ca06aa8c2da4c
+(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient")
