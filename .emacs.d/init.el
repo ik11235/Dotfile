3abb4c18f642d027b55df32f35b7d61f255b8b2d
ik-fib <ik11235@gmail.com>
2014-04-06 15:45:32 +0900

    fix install process to init-loader.el for package.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,32 @@
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path)))
 
+;;init-loaderをpackage経由でインストールするため、packageの設定、必要パッケージの自動インストール
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    init-loader
+    helm
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
+
+
+
+
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
