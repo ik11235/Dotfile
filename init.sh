8fe97a8eb8fdd7a4c188b1bab6a15534a313b1c3
ik-fib <ik11235@gmail.com>
2014-11-14 02:48:21 +0900

    インストール対象にtmux zshを追加 およびインストール手順の変更

--- init.sh
+++ init.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 cd `dirname $0`
 LINKFILE=(".emacs.d" ".zshrc" ".zsh.d" ".aspell.conf")
-INSTALL_PAK=("aspell")
+INSTALL_PAK=("zsh" "aspell" "tmux")
 
 for LINK in ${LINKFILE[@]}
 do
     ln -s `pwd`/${LINK} ${HOME}/${LINK}
 done
 
-for PAK in ${INSTALL_PAK[@]}
-do
-    if [ `uname` = "Darwin" ]; then
+if [ `uname` = "Darwin" ]; then
 	#mac用のコード
-	brew install --with-lang-en ${PAK}
-    elif [ `uname` = "Linux" ]; then
+	for PAK in ${INSTALL_PAK[@]}
+	do
+		brew install ${PAK}
+	done
+elif [ `uname` = "Linux" ]; then
 	#Linux用のコード
 	#TODO ディストリビューションごとにyumとaptを制御する
-	sudo apt-get install ${PAK}
-    fi
-done
+	sudo apt-get install ${INSTALL_PAK[@]}
+fi
