66e97d1aa67be23fbb59cfad1c2f921dc1501595
ik-fib <ik11235@gmail.com>
2014-12-25 00:28:06 +0900

    yasnippetの表示をhelm形式に変更

--- .emacs.d/inits/22-yasnippet.el
+++ .emacs.d/inits/22-yasnippet.el
@@ -15,3 +15,24 @@
 (define-key yas-minor-mode-map (kbd "C-x y n") 'yas-new-snippet)
 ;; 既存スニペットを閲覧・編集する
 (define-key yas-minor-mode-map (kbd "C-x y v") 'yas-visit-snippet-file)
+
+
+;; helm interface
+;; http://d.hatena.ne.jp/syohex/20121207/1354885367
+;; https://github.com/sugyan/dotfiles/blob/master/.emacs.d/inits/04-yasnippet.el
+(eval-after-load "helm"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (helm-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*helm yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))))
