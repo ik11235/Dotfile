d4cea1e39346f02edf6780774e5092a74aa038ac
ik-fib <ik11235@gmail.com>
2015-01-17 16:56:01 +0900

    helpでminibuffer で C-h が効かない問題に対応

--- .emacs.d/inits/21-helm.el
+++ .emacs.d/inits/21-helm.el
@@ -4,6 +4,9 @@
 (define-key global-map (kbd "C-x b") 'helm-for-files)
 (define-key global-map (kbd "C-x C-b") 'helm-for-files)
 
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+
 ;http://konbu13.hatenablog.com/entry/2014/01/15/223014
 ;http://fukuyama.co/nonexpansion
 ;; TABで補完
