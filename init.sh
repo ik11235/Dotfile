cbcb5dcc7fa1c2b050f6df9b4860a048a5bfc0a6
ik-fib <ik11235@gmail.com>
2017-01-11 17:38:42 +0900

    デフォルト用.textlintrc追加

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
-LINKFILE=(".emacs.d/" ".zshrc" ".zsh.d/" ".aspell.conf" ".gitignore")
+LINKFILE=(".emacs.d/" ".zshrc" ".zsh.d/" ".aspell.conf" ".gitignore" ".textlintrc")
 INSTALL_PAK=("texlive-extra-utils" "texlive-lang-japanese" "zsh" "aspell" "tmux" "make" "g++" "ruby-all-dev" "bundler" "command-not-found" "npm")
 
 for LINK in ${LINKFILE[@]}
