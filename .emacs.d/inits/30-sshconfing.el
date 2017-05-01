bcf2bda25a0406bdc8d8ba0464b1dc838d174098
ik-fib <ik11235@gmail.com>
2017-05-01 10:51:24 +0900

    ssh config用設定追加

---
+++ .emacs.d/inits/30-sshconfing.el
@@ -0,0 +1,7 @@
+(autoload 'ssh-config-mode "ssh-config-mode" t)
+(add-to-list 'auto-mode-alist '("/\\.ssh/config\\'"     . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("/sshd?_config\\'"      . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("/\\.ssh/hosts/.+\\'"   . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("/known_hosts\\'"       . ssh-known-hosts-mode))
+(add-to-list 'auto-mode-alist '("/authorized_keys2?\\'" . ssh-authorized-keys-mode))
+(add-hook 'ssh-config-mode-hook 'turn-on-font-lock)
