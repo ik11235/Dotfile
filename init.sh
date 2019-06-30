f4fc98453b480b2713b5ca30ab76cf11a3bd05c9
ik-fib <ik11235@gmail.com>
2019-06-30 11:57:36 +0900

    不要なコマンドを削除 rbenv等の切り替え

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd `dirname $0`
 LINKFILE=(".emacs.d/" ".zshrc" ".zsh.d/" ".aspell.conf" ".gitignore" ".textlintrc")
-INSTALL_PAK=("zsh" "aspell" "tmux" "npm" "coreutils")
+INSTALL_PAK=("zsh" "aspell" "tmux" "npm" "coreutils" "rbenv" "ruby-build")
 
 for LINK in ${LINKFILE[@]}
 do
@@ -35,3 +35,12 @@ chsh -s ${ZSHPATH}
 
 # textlintのインストール
 npm i -g textlint textlint-rule-preset-japanese textlint-rule-prh textlint-rule-ja-no-redundant-expression
+
+# command-not-foundのインストール
+brew tap homebrew/command-not-found
+
+# rbenvで最新バージョンをインストール
+## https://mawatari.jp/archives/install-latest-stable-version-of-ruby-using-rbenv
+RUBY_VER=`rbenv install -l | grep -v - | tail -1 | tr -d ' '`
+rbenv install $RUBY_VER
+rbenv global $RUBY_VER
