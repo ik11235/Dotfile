d730047e6f26557b446662b7579be599aaaff578
ik-fib <ik11235@gmail.com>
2020-02-12 17:47:17 +0900

    fish回りの更新をする関数を追加

---
+++ .config/fish/functions/fishes_update.fish
@@ -0,0 +1,9 @@
+function fishes_update -d "fish関連の更新をする(fisherやfishの補完等)"
+    # fisher関係のupdate
+    fisher self-update
+    fisher
+
+    # fishの補完を更新
+    ## https://fishshell.com/docs/current/commands.html#fish_update_completions
+    fish_update_completions
+end
