a7dbd6d4132bf31eae998c83fb071b823c563ee3
ik-fib <ik11235@gmail.com>
2020-06-28 14:13:10 +0900

    font自動install

--- init.sh
+++ init.sh
@@ -65,3 +65,15 @@ rbenv global $RUBY_VER
 
 # private file作成
 touch ${HOME}/.zsh.d/zshrc_mac_private
+
+# powerline-fonts install
+(
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+)
