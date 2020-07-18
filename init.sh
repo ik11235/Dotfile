8781537866980656bb175147f6466edf625016d9
ik-fib <ik11235@gmail.com>
2020-07-19 00:33:02 +0900

    リンク作成を別ファイル化

--- init.sh
+++ init.sh
@@ -2,35 +2,7 @@
 
 cd "$(dirname "$0")"
 
-function link_dir_file() {
-    TARGET_DIR=$1
-    LINK_TARGET_PREFIX=$2
-
-    cd "$TARGET_DIR"
-
-    # SC2045
-    for LINK in *
-    do
-      [[ -e "$LINK" ]] || break
-      # 既にリンク済みの場合、そのディレクトリの階層の下に更にリンクされるので事前にディレクトリの存在有無をチェックする
-      # ファイルの場合、ln側で自動的に重複で止まるのでチェックは不要
-      if [ -d "${LINK_TARGET_PREFIX}${LINK}" ]; then
-        echo "${LINK_TARGET_PREFIX}${LINK}: Directory exists."
-        continue;
-      fi
-      ln -sv "$(pwd)/${LINK}" "${LINK_TARGET_PREFIX}${LINK}"
-    done
-
-    cd ..
-}
-
-## 先頭に.をつけてリンクする対象一覧
-DOT_LINK_FILE_DIR="DOT_LINK_TARGET"
-link_dir_file "${DOT_LINK_FILE_DIR}" "${HOME}/."
-
-## 先頭に.をつけず、そのままの名前でリンクする対象一覧
-LINK_FILE_DIR="LINK_TARGET"
-link_dir_file "${LINK_FILE_DIR}" "${HOME}/"
+/bin/bash .link_dir_and_file.sh
 
 echo "Do you want to install a package ? [Y/n]"
 read -r ANSWER
