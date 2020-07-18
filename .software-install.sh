bc70b27c85ec62c9ab98a57f8f0c5357ab374c4f
ik-fib <ik11235@gmail.com>
2020-07-19 00:41:58 +0900

    unameを一度だけ実行するように & 想定外のOSのときはその旨を出力

--- .software-install.sh
+++ .software-install.sh
@@ -2,11 +2,12 @@
 
 echo "Do you want to install a package ? [Y/n]"
 read -r ANSWER
-
 ANSWER=$(echo "$ANSWER" | tr y Y)
+
+OS_TYPE=$(uname)
 case $ANSWER in
 "" | Y*)
-  if [ "$(uname)" = "Darwin" ]; then
+  if [ "${OS_TYPE}" = "Darwin" ]; then
     # 実行環境がLinuxの場合のインストール処理
 
     # brewのインストールの有無をチェック
@@ -19,12 +20,18 @@ case $ANSWER in
 
     # この処理の前にlink済みであるはずのBrewfileを元にinstall
     brew bundle --global
-  elif [ "$(uname)" = "Linux" ]; then
+  elif [ "${OS_TYPE}" = "Linux" ]; then
     # 実行環境がLinuxの場合のインストール処理
     # TODO: ディストリビューションごとにyumとaptを制御する
 
     echo "Linux sh"
     echo "${INSTALL_PAK[@]}"
+  else
+    # 上記に当てはまらない環境の場合
+    # TODO: 必要に応じて処理を書く
+    echo "It's running on an unexpected OS."
+    echo "uname result: ${OS_TYPE}"
+    return 1
   fi
   ;;
 *)
