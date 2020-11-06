d6658416873c1b67e5f96a5d8d44639bace9350b
ik11235@gmail.com <yuichi.ikeda@stadium.co.jp>
2020-11-06 13:54:07 +0900

    stringの比較結果をdev/nullへ

--- DOT_LINK_TARGET/config/fish/config.fish
+++ DOT_LINK_TARGET/config/fish/config.fish
@@ -40,7 +40,7 @@ end
 set NOT_ANYENV_PATH ""
 
 for tmp_path in (string split : $PATH)
-    if ! string match '*/.anyenv/*' $tmp_path
+    if ! string match '*/.anyenv/*' $tmp_path >> /dev/null
         set NOT_ANYENV_PATH $NOT_ANYENV_PATH $tmp_path
     end
 end
