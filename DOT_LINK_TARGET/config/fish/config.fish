8dfc137c5a6049ad10218a5cf27dbe2f6f2f918a
ik11235@gmail.com <yuichi.ikeda@stadium.co.jp>
2020-11-06 13:37:20 +0900

    brew doctorの警告をfishでも出さないように

--- DOT_LINK_TARGET/config/fish/config.fish
+++ DOT_LINK_TARGET/config/fish/config.fish
@@ -31,3 +31,25 @@ function save_history --on-event fish_preexec
     history save
     history marge
 end
+
+# anyenvのPATHをbrewの実行時の対象から除外する
+## https://www.null-engineer.com/2019/02/08/anyenv%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E6%B8%88%E3%81%BF%E3%81%AEmac%E3%81%A7brew-doctor%E3%82%92%E5%AE%9F%E8%A1%8C%E3%81%97%E3%81%9F%E3%82%89warning%E3%81%8C%E5%87%BA%E3%82%8B/
+## phpenv等でも出るらしいので、anyenv管理のものは一律除外
+## https://qiita.com/takc923/items/45386905f70fde9af0e7
+
+#set EXCLUSION_PATH (echo ~/.anyenv/envs/pyenv/shims)
+#alias brew="env PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin brew"
+
+#echo $EXCLUSION_PATH
+#echo $PATH
+#echo (string replace -a $EXCLUSION_PATH "" $PATH)
+
+set NOT_ANYENV_PATH ""
+
+for tmp_path in (string split : $PATH)
+          if ! string match '*/.anyenv/*' $tmp_path
+           set NOT_ANYENV_PATH $NOT_ANYENV_PATH $tmp_path
+           end
+     end
+
+     alias brew="env PATH=$NOT_ANYENV_PATH brew"
