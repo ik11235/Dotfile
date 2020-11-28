9c3480471027bf4ca7e29f34a07eb98ab2318c62
ik-fib <ik11235@gmail.com>
2020-11-28 13:34:44 +0900

    peco関係の設定を削除

--- DOT_LINK_TARGET/config/fish/config.fish
+++ DOT_LINK_TARGET/config/fish/config.fish
@@ -1,15 +1,8 @@
 #https://qiita.com/naname/items/00d4c81a98c017b0fb43
 
 # ~/.config/fish/config.fishに追加
-#fisherパッケージoh-my-fish/plugin-pecoの設定
-function fish_user_key_bindings
-  bind \cr peco_select_history # Bind for prco history to Ctrl+r
-end
-
-
-# ~/.config/fish/config.fishに追加
 #fisherパッケージdecors/fish-ghqの設定
-set GHQ_SELECTOR peco
+set GHQ_SELECTOR fzf
 
 
 # google cloud sdkのPATH
