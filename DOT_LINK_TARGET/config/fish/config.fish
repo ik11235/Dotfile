b0f57ba79af8fec2d985c4a2b7b73e4da33b4b6f
ik-fib <ik11235@gmail.com>
2025-02-07 21:25:16 +0900

    asdfの設定を最新の公式サイトに合わせて更新

--- DOT_LINK_TARGET/config/fish/config.fish
+++ DOT_LINK_TARGET/config/fish/config.fish
@@ -32,8 +32,22 @@ function save_history --on-event fish_preexec
     history marge
 end
 
+
 # asdf
-#source (brew --prefix asdf)"/libexec/asdf.fish"
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 . ~/.asdf/plugins/golang/set-env.fish
 
 # anyenvのPATHをbrewの実行時の対象から除外する
